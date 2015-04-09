0b04ed2c125b5ff863c25fc480c0d01af7fcbd66
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-04-09 15:29:35 +0200

    Only 1d6+ roll is currently supported

--- Drd/DiceRoll/Templates/Roll1d6Plus.php
+++ Drd/DiceRoll/Templates/Roll1d6Plus.php
@@ -5,13 +5,13 @@ use Drd\DiceRoll\Dice;
 use Drd\DiceRoll\Roll;
 use Granam\Strict\Integer\StrictInteger;
 
-class Roll2d6Plus extends Roll
+class Roll1d6Plus extends Roll
 {
     public function __construct()
     {
         parent::__construct(
             new Dice(new StrictInteger(1), new StrictInteger(6)),
-            new StrictInteger(2),
+            new StrictInteger(1),
             new StrictInteger(6)
         );
     }
---
+++ Tests/Drd/DiceRoll/Templates/Roll1d6PlusTest.php
@@ -0,0 +1,53 @@
+<?php
+namespace Drd\DiceRoll\Templates;
+
+class Roll1d6PlusTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function can_create_instance()
+    {
+        $instance = new Roll1d6Plus();
+        $this->assertNotNull($instance);
+
+        return $instance;
+    }
+
+    /**
+     * @param Roll1d6Plus $roll1d6Plus
+     *
+     * @test
+     * @depends can_create_instance
+     */
+    public function minimum_is_one(Roll1d6Plus $roll1d6Plus)
+    {
+        $this->assertSame(1, $roll1d6Plus->getDice()->getMinimum()->getValue());
+    }
+
+    /**
+     * @param Roll1d6Plus $roll1d6Plus
+     *
+     * @test
+     * @depends can_create_instance
+     */
+    public function maximum_is_six(Roll1d6Plus $roll1d6Plus)
+    {
+        $this->assertSame(6, $roll1d6Plus->getDice()->getMaximum()->getValue());
+    }
+
+    /**
+     * @param Roll1d6Plus $roll1d6Plus
+     *
+     * @test
+     * @depends can_create_instance
+     */
+    public function at_least_one_roll(Roll1d6Plus $roll1d6Plus)
+    {
+        $this->assertSame(1, $roll1d6Plus->getRollNumber()->getValue());
+        $this->assertSame(6, $roll1d6Plus->getRepeatOnValue()->getValue());
+        $this->assertGreaterThanOrEqual($roll1d6Plus->getDice()->getMinimum()->getValue(), $roll1d6Plus->roll());
+        $this->greaterThanOrEqual(1, count($roll1d6Plus->getLastDiceRolls()));
+    }
+}
